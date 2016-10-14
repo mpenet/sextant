fb4356ca6358ac490ed27cfc53e79446b7953930
Max Penet <m@qbits.cc>
2016-10-14 15:29:07 +0200

    housekeeping

--- src/clj/qbits/sextant.clj
+++ src/clj/qbits/sextant.clj
@@ -1,14 +1,7 @@
-(ns qbits.sextant
-  (:import
-   (com.taykey.twitterlocationparser.dto.Location)
-   (com.taykey.twitterlocationparser DefaultLocationParser)
-   (com.taykey.twitterlocationparser.dto LocationType)))
+(ns qbits.sextant)
 
 (defrecord Location [name state-code country-code type
                      population latitude longitude])
 
 (defprotocol LocationFinder
   (find-location [this x]))
-
-;; (clojure.pprint/pprint (find-location p "geneve"))
-;; (clojure.pprint/pprint (find-location p "paris"))
