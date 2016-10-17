169b1c54b71557a97a21c0b3552e73595d54f8ec
Max Penet <m@qbits.cc>
2016-10-17 08:55:41 +0200

    kill useless plugin

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
                  [com.stuartsierra/component "0.3.1"]
                  [org.slf4j/slf4j-api "1.7.21"]
                  [org.slf4j/slf4j-log4j12 "1.7.21"]]
-  :plugins [[lein-junit "1.1.8"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :global-vars {*warn-on-reflection* true})
