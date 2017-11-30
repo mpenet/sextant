daadc166564d8333c92511cdabc0232aa97fa3ee
Max Penet <m@qbits.cc>
2017-11-30 09:23:35 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cc.qbits/sextant "1.0.2"
+(defproject cc.qbits/sextant "1.0.3"
   :description "Decode locations from things"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [com.stuartsierra/component "0.3.1"]
                  [org.slf4j/slf4j-api "1.7.21"]
                  [org.slf4j/slf4j-log4j12 "1.7.21"]]
