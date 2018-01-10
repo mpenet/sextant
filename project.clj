d6122dd7e1229ac5b9d76e32315bdbe6abe3b4f4
Reynald Borer <reynald.borer@paper.li>
2018-01-10 09:02:41 +0100

    Bump clojure, component & slf4j versions

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject cc.qbits/sextant "1.0.3"
   :description "Decode locations from things"
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [org.slf4j/slf4j-api "1.7.21"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [org.slf4j/slf4j-api "1.7.25"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :global-vars {*warn-on-reflection* true}
-  :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.21" :scope "runtime"]]}})
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.25" :scope "runtime"]]}})
