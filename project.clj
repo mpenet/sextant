d3f280ba4b6f8523d5cd31933f5db4eb456c6570
Reynald Borer <reynald.borer@paper.li>
2018-01-10 08:58:06 +0100

    Move org.slf4j/slf4j-log4j12 into :dev profile (fixes #1)
    This dependency is superfluous as it is the binding between slf4j logging API and Log4j 1.2 logger. Thus it forces projects using this dependency to either use Log4j 1.2 or exclude this dependency.
    
    The dependency is also scoped as runtime so it's not accessible to the code but available at runtime.

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Decode locations from things"
   :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [com.stuartsierra/component "0.3.1"]
-                 [org.slf4j/slf4j-api "1.7.21"]
-                 [org.slf4j/slf4j-log4j12 "1.7.21"]]
+                 [org.slf4j/slf4j-api "1.7.21"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.21" :scope "runtime"]]}})
